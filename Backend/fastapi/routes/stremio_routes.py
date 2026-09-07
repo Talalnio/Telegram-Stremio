f92af62bce747bc7b0b73ed59b0c925887901e29
Talalnio <harbitml@gmail.com>
2026-09-07 13:32:56 +0300

    Update stremio_routes.py

--- Backend/fastapi/routes/stremio_routes.py
+++ Backend/fastapi/routes/stremio_routes.py
@@ -1108,7 +1108,7 @@ async def get_streams(
         if name_count[s["name"]] > 1:
             seen[s["name"]] = seen.get(s["name"], 0) + 1
             s["name"] = f"{s['name']} ({seen[s['name']]})"
-#    streams.insert(0, _donation())
+    streams.insert(0, _donation())
     return {"streams": streams}
 
 #----- Configure/install landing page rendered as HTML for a token
