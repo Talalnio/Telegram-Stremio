e941c6a51979a6acb907391b72f67006786bd3a5
Talalnio <harbitml@gmail.com>
2026-09-07 13:19:19 +0300

    Update sr

--- Backend/fastapi/routes/stremio_routes.py
+++ Backend/fastapi/routes/stremio_routes.py
@@ -34,8 +34,8 @@ ADDON_NAME = "TG"
 ADDON_VERSION = __version__
 PAGE_SIZE = 15
 
-def _donation():
-    return {"name": "⭐ Donation needed.", "title": "Click here to donate to keep the project alive.", "externalUrl": "https://donate.weebzonex.workers.dev"}
+# def _donation():
+#    return {"name": "⭐ Donation needed.", "title": "Click here to donate to keep the project alive.", "externalUrl": "https://donate.weebzonex.workers.dev"}
 
 def build_proxy_url(original_url: str) -> str | None:
     settings = SettingsManager.current()
